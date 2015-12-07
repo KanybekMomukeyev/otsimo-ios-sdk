e066d2134651110f866d3bc5fd1b3bc3d351dfa6
Sercan Degirmenci <degirmencisercan@gmail.com>
2015-12-07 12:15:16 +0200

    initial code

---
+++ Source/Otsimo.swift
@@ -0,0 +1,13 @@
+//
+//  Otsimo.swift
+//  AccountSDK
+//
+//  Created by Sercan Degirmenci on 07/12/15.
+//  Copyright © 2015 Otsimo. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Account{
+}
