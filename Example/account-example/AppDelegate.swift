e34cd9dbb0a2854b257d0493309a0f274cf33330
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-02-29 18:03:18 +0200

    format code

--- Example/account-example/AppDelegate.swift
+++ Example/account-example/AppDelegate.swift
@@ -11,67 +11,64 @@ import OtsimoSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         var keys: NSDictionary?
-        
+
         if let path = NSBundle.mainBundle().pathForResource("keys", ofType: "plist") {
             keys = NSDictionary(contentsOfFile: path)
         }
         if let dict = keys {
             clientID = dict["OtsimoClientID"] as! String
         }
-        
+
         otsimo.sessionStatusChanged = onSessionStatusChanged
 
         #if DEBUG
-        Otsimo.config(ClientConfig.development(clientID, host: devHost))
+            Otsimo.config(ClientConfig.development(clientID, host: devHost))
         #else
-        Otsimo.config(ClientConfig.development(clientID, host: devHost))
+            Otsimo.config(ClientConfig.development(clientID, host: devHost))
         #endif
         return true
     }
-    
+
     func onSessionStatusChanged(ses: Session?) {
-        let (_,e) = otsimo.startWatch(watchCallback)
-        print("WatchCreate: \(e)")
+        let (w, e) = otsimo.startWatch(watchCallback)
+        print("WatchCreate: \(w) \(e)")
     }
-    
+
     func application(application: UIApplication,
         openURL url: NSURL,
         sourceApplication: String?,
         annotation: AnyObject) -> Bool {
-        
-        if (url.scheme == "comotsimosdk_example") {
-            otsimo.handleOpenURL(url)
-        }
-        return true
+
+            if (url.scheme == "comotsimosdk_example") {
+                otsimo.handleOpenURL(url)
+            }
+            return true
     }
-    
+
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-    
+
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-    
+
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-    
+
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-    
+
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    
 }
-
--- Example/account-example/TestHomeController.swift
+++ Example/account-example/TestHomeController.swift
@@ -10,7 +10,7 @@ import UIKit
 import OtsimoSDK
 import OtsimoApiGrpc
 
-func watchCallback(event:OTSWatchEvent){
+func watchCallback(event: OTSWatchEvent) {
     print("Watch: \(event)")
 }
 
@@ -18,42 +18,41 @@ class TestHomeController: UITableViewController {
     var testEntries: [ApiTest] = apiTestScenes
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-        
+
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         otsimo.sessionStatusChanged = onSessionStatusChanged
     }
-    
+
     func onSessionStatusChanged(ses: Session?) {
         self.tableView.reloadData()
-        let (_,e) = otsimo.startWatch(watchCallback)
+        let (_, e) = otsimo.startWatch(watchCallback)
         print("Watch: \(e)")
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     // MARK: - Table view data source
-    
+
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return testEntries.count
     }
-    
-    
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("test_reuse_identifier", forIndexPath: indexPath)
-        
+
         // Configure the cell...
         let test = testEntries[indexPath.row]
         cell.textLabel?.text = test.title
@@ -91,20 +90,20 @@ class TestHomeController: UITableViewController {
             performSegueWithIdentifier(test.segmentName, sender: tableView)
         }
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if let id = segue.identifier {
-            
-            otsimo.analytics.customEvent("test:scene", payload: ["identifier":id])
-            
+
+            otsimo.analytics.customEvent("test:scene", payload: ["identifier": id])
+
             if id == "editchildgamestest" {
                 let cic = segue.destinationViewController as! ChildListViewController
                 cic.nextSegue = "getchildtest"
