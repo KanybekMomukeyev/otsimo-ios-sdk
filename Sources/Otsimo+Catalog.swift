f3efe2254948fa5e4c894cb5a77621e53e0acaad
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-03-09 13:07:57 +0200

    return cache result if remote call failed

--- Sources/Otsimo+Catalog.swift
+++ Sources/Otsimo+Catalog.swift
@@ -10,24 +10,35 @@ import Foundation
 import OtsimoApiGrpc
 
 extension Otsimo: CatalogApi {
-    
+
     // Catalog
     public func getCatalog(handler: (OTSCatalog?, OtsimoError) -> Void) {
-        Otsimo.sharedInstance.cache.fetchCatalog() {cat in
-            if let catalog = cat {
-                handler(catalog, OtsimoError.None)
-            } else {
-                self.getCatalogFromRemote(handler)
+        self.getCatalogFromRemote { c, e in
+            switch (e) {
+            case .None:
+                if let catalog = c {
+                    handler(catalog, e)
+                    return
+                }
+                fallthrough
+            default:
+                Otsimo.sharedInstance.cache.fetchCatalog() { cat in
+                    if let catalog = cat {
+                        handler(catalog, .None)
+                    } else {
+                        handler(nil, .General(message: "Not Found"))
+                    }
+                }
             }
         }
     }
-    
+
     private func getCatalogFromRemote(handler: (OTSCatalog?, OtsimoError) -> Void) {
         if let connection = connection {
             let req = OTSCatalogPullRequest()
             if let ses = session {
                 req.profileId = ses.profileID
-                connection.getCurrentCatalog(ses, req: req) {res, err in
+                connection.getCurrentCatalog(ses, req: req) { res, err in
                     if let catalog = res {
                         Otsimo.sharedInstance.cache.cacheCatalog(catalog)
                         handler(catalog, OtsimoError.None)
@@ -43,4 +54,3 @@ extension Otsimo: CatalogApi {
         }
     }
 }
-
